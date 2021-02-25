4e6993c78c3515cd22771352216d6f20c3493544
LiHua Anderson <lihua.anderson@gmail.com>
2021-02-25 02:15:31 -0800

    Added script for a timer countdown to appear once Quiz starts. Also began work in index.html to show highscores once quiz is over

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,28 +2,36 @@ const startBtn = document.getElementById('start');
 const questionsEl = document.getElementById('questions');
 const questionEl = document.getElementById('question');
 const AnswerEl = document.getElementById('answerbtn');
+const timerEl = document.getElementById("countdown");
 
 let randomQuestion, chosenQuestion
 
+var timeLeft = 10;
 
-startBtn.addEventListener('click', startQuiz);
+function setCountdown() {
+  var timerInterval = setInterval(function() {
+    timeLeft--;
+    timerEl.textContent = timeLeft + " seconds remaining.";
 
-function startQuiz() {
-    console.log('Started');
-    startBtn.classList.add('hidden');
-    questionsEl.classList.remove('hidden');
-    randomQuestion = questions.sort(() => Math.random() - .5);
-    chosenQuestion = 0;
-    newQuestion();
+    if(timeLeft === 0) {
+      clearInterval(timerInterval);
+      sendMessage();
+    }
+
+  }, 1000);
+};
+
+function sendMessage() {
+  timerEl.textContent = "Times Up!";
 };
 
 function newQuestion() {
     resetQuestion()
- showQuestion(randomQuestion[chosenQuestion])
+    showQuestion(randomQuestion[chosenQuestion])
 };
 
-function showQuestion (question) {
-questionEl.innerText = question.question;
+function showQuestion(question) {
+    questionEl.innerText = question.question;
 };
 
 function resetQuestion() {
@@ -38,10 +46,22 @@ const questions = [
     {
         question: 'What is a word used to declare a variable?',
         answers: [
-            { text: 'vari', correct: false},
-            { text: 'y', correct: false},
-            { text: 'var', correct: true},
-            { text: 'value', correct: false}
+            { text: 'vari', correct: false },
+            { text: 'y', correct: false },
+            { text: 'var', correct: true },
+            { text: 'value', correct: false }
         ]
     }
 ]
+
+startBtn.addEventListener('click', startQuiz);
+
+function startQuiz() {
+    console.log('Started');
+    startBtn.classList.add('hidden');
+    questionsEl.classList.remove('hidden');
+    randomQuestion = questions.sort(() => Math.random() - .5);
+    chosenQuestion = 0;
+    newQuestion();
+    setCountdown();
+};
